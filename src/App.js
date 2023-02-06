d5c03016a4769988f50b5dab36ce3b178c535648
TheLuckyMojo <ethanmorales0730@gmail.com>
2023-02-05 21:14:38 -0800

    6.3 - adding routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
 import NavBar from "./components/navbar";
 import Products from "./components/products";
 import Posts from "./components/posts";
@@ -13,6 +14,12 @@ class App extends Component {
     return (
       <div>
         <NavBar />
+        <div className='content'>
+          <Route path='/products' Component={Products} />
+          <Route path='/posts' Component={Posts} />
+          <Route path='/admin' Component={Dashboard} />
+          <Route path='/' Component={Home} />
+        </div>
       </div>
     );
   }
